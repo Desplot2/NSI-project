38a9455b1cd3e928c75d9f37d2c59a743238a427
Desplot2 <65451899+Desplot2@users.noreply.github.com>
2020-05-16 16:54:42 +0200

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,14 @@
+var elements = document.getElementsByClassName('white');
+for (var i in elements) {
+  if (elements.hasOwnProperty(i)) {
+    elements[i].className = 'coral';
+  }
+}
 
+
+var elements = document.getElementsByClassName('coral');
+for (var i in elements) {
+  if (elements.hasOwnProperty(i)) {
+    elements[i].className = 'white';
+  }
+}
